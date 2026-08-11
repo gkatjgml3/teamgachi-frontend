9449cf82357ea289a9cd2162df714e5703d2044d
박서아 <seoa100128@gmail.com>
2026-08-11 22:03:10 +0900

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,5 +1,5 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-import { SUPABASE_PUBLISHABLE_KEY, SUPABASE_URL } from './js/config.js';
+import { SUPABASE_PUBLISHABLE_KEY, SUPABASE_URL } from './config.js';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
