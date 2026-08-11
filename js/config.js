47ddb45277ac3e43f8f696800a11797ec3bb9a70
박서아 <seoa100128@gmail.com>
2026-08-11 22:03:46 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 export const SUPABASE_URL = 'https://yezdalggrwjtjemkcehj.supabase.co';
-export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_uU2CNA5o-NU2slxQWgp7tQ_Ij1sMO-Z';
+export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_uU2CNA5o-NU2slxQwgp7tQ_Ij1sM0-Z';
